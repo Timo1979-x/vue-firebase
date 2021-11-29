c7caf0c672cbbade5a343b3b256fb5d783753b69
Tim Lukashevich <efreet@tut.by>
2021-11-29 07:21:25 +0300

    Lesson 100. All previous lessons deleted. They all present in initial commit

--- lesson007/app.js
+++
@@ -1,7 +0,0 @@
-console.log("hello, Vue2");
-
-const app = Vue.createApp({
-  // template:'<h2>App</h2>'
-});
-
-app.mount("#app");
--- lesson036/src/components/Results.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <p>Reaction time: {{ score }} ms</p>
-</template>
-
-<script>
-export default {
-  props: ["score"],
-};
-</script>
