256cf9e86bfaa9c762df3baf924e61fd683f9c7a
Tim Lukashevich <efreet@tut.by>
2021-11-29 21:43:46 +0300

    lesson103

---
+++ live-chat/src/components/LoginForm.vue
@@ -0,0 +1,24 @@
+<template>
+  <form @submit.prevent="login">
+    <input type="email" required placeholder="email" v-model="email" />
+    <input type="password" required placeholder="password" v-model="password" />
+    <button>Login</button>
+  </form>
+</template>
+
+<script>
+import { ref } from "@vue/reactivity";
+export default {
+  setup() {
+    // refs
+    const email = ref("");
+    const password = ref("");
+    const login = () => {
+      console.log(email.value, password.value);
+    };
+    return { email, password, login };
+  },
+};
+</script>
+
+<style></style>
---
+++ live-chat/src/components/SignupForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <form @submit.prevent="submit">
+    <input
+      type="text"
+      required
+      placeholder="Display name"
+      v-model="displayName"
+    />
+    <input type="email" required placeholder="email" v-model="email" />
+    <input type="password" required placeholder="password" v-model="password" />
+    <button>Sign up</button>
+  </form>
+</template>
+
+<script>
+import { ref } from "@vue/reactivity";
+export default {
+  setup() {
+    // refs
+    const displayName = ref("");
+    const email = ref("");
+    const password = ref("");
+    const submit = () => {
+      console.log(displayName.value, email.value, password.value);
+    };
+    return { displayName, email, password, submit };
+  },
+};
+</script>
+
+<style></style>
