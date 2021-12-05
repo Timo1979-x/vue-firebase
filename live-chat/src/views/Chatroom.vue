4a77956ca8a87204d7ac0351ea3ffe9721f7eb75
Tim Lukashevich <efreet@tut.by>
2021-12-05 14:52:30 +0300

    lesson115

--- live-chat/src/views/Chatroom.vue
+++ live-chat/src/views/Chatroom.vue
@@ -6,7 +6,20 @@
 
 <script>
 import Navbar from "../components/Navbar.vue";
+import { useRouter } from "vue-router";
+import getUser from "../composables/getUser";
+import { watch } from "@vue/runtime-core";
+
 export default {
   components: { Navbar },
+  setup() {
+    let router = useRouter();
+    let { user } = getUser();
+    watch(user, () => {
+      if (!user.value) {
+        router.push({ name: "Welcome" });
+      }
+    });
+  },
 };
 </script>
